de5eddeb749831143e33ec58f787d2a8686162c0
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-09 20:48:55 +0530

    accept the client and send all 200ok req

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import socket
 
 
-
-
-
 def main():
     print("Hello from http-web-server!")
     server_socket = socket.create_server(("localhost", 8000),reuse_port=True)
-    server_socket.accept() # wait for client
+    #server_socket.accept() # wait for client
+    
+    server_socket.listen()
+    
+    client, addr = server_socket.accept()
+    client.sendall("HTTP/1.1 200 OK\r\n\r\n".encode("utf-8"))
+    
 
 
 if __name__ == "__main__":
