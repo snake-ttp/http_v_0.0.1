d9bc9df7492c8d900ef1eae90e1ab73d2065d942
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-09 20:41:56 +0530

    create the server

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import socket
+
+
+
+
+
 def main():
     print("Hello from http-web-server!")
+    server_socket = socket.create_server(("localhost", 8000),reuse_port=True)
+    server_socket.accept() # wait for client
 
 
 if __name__ == "__main__":
