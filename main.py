f1cae0daf39861bb5f14129121d24c4a3efc9dfd
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-09 20:59:46 +0530

    handle exception

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import socket
 
+def handle_request(client_socket: socket.socket):
+    client_socket.recv(1024)
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    client_socket.send(response.encode())
+
 
 def main():
     print("Hello from http-web-server!")
-    server_socket = socket.create_server(("localhost", 8000),reuse_port=True)
+    server_socket = socket.create_server(("localhost", 6000),reuse_port=True)
     #server_socket.accept() # wait for client
+    print ("Server is running on port : 6000")
     
     server_socket.listen()
+    try:
+        client, addr = server_socket.accept()
+        print(f"Connection from {addr} has been established")
+        
+    except Exception as e:
+        print(f"Error {e}")
     
-    client, addr = server_socket.accept()
-    client.sendall("HTTP/1.1 200 OK\r\n\r\n".encode("utf-8"))
     
 
 
