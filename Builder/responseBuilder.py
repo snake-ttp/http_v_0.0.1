e78eaf299996cb3abc805f13cc85e3e44c21f567
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-13 21:39:48 +0530

    update builder fn

--- Builder/responseBuilder.py
+++ Builder/responseBuilder.py
@@ -1,4 +1,4 @@
-def build_response(body: str, status="200 OK", content_type="text/plain"):
+def build_response(body: str="", status="200 OK", content_type="text/plain"):
     return (
         f"HTTP/1.1 {status}\r\n"
         f"Content-Type: {content_type}\r\n"
