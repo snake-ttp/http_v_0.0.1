03778ce3968ed4f449323106cdb0649f24e4639e
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-17 20:28:56 +0530

    builder added content encoding

--- Builder/responseBuilder.py
+++ Builder/responseBuilder.py
@@ -1,9 +1,23 @@
-def build_response(body: str="", status="200 OK", content_type="text/plain"):
-    return (
-        f"HTTP/1.1 {status}\r\n"
-        f"Content-Type: {content_type}\r\n"
-        f"Content-Length: {len(body.encode())}\r\n"
+def build_response(content_encoding, body: str="", status="200 OK", content_type="text/plain" ):
+    response:str = f"HTTP/1.1 {status}\r\n"
+    
+    if content_type:
+        response += (
+            f"Content-Type: {content_type}\r\n"
+            f"Content-Length: {len(body.encode())}\r\n"
+        )
+    if content_encoding:
+        response += (
+            f"Content-Encoding: gzip"
+        )
+    
+    response += (
         f"Connection: close\r\n"
         f"\r\n"
         f"{body}"
     )
+    
+    return response
+    
+    
+    
